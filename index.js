113a04e7e790de56f782de378998836cd1428da6
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-01-08 13:04:57 +0100

    fix card

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const main = document.getElementById("main");
 
-main.addEventListener("click", function(event){
-    console.log(event.target.innerText)
-})
+const cards = document.querySelectorAll(".job-card");
+const tags = document.querySelectorAll(".tag");
+
+tags.forEach(tag => {
+  tag.addEventListener("click", () => {
+    const value = tag.textContent.toLowerCase();
+
+    cards.forEach(card => {
+      const role = card.dataset.role;
+      const level = card.dataset.level;
+      const languages = card.dataset.languages.split(",");
+      const tools = card.dataset.tools
+        ? card.dataset.tools.split(",")
+        : [];
+
+      const match =
+        role === value ||
+        level === value ||
+        languages.includes(value) ||
+        tools.includes(value);
+
+        console.log(match);
+
+      card.style.display = match ? "block" : "none";
+    });
+  });
+});
